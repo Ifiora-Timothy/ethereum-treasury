50b602a382d01df069c4fda36005accb886a0472
Ifiora Timothy <ifioratimothy2001@gmail.com>
2024-12-18 11:06:26 +0100

    initial commit

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test,console} from "lib/forge-std/src/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
+    
 
     function setUp() public {
         counter = new Counter();
